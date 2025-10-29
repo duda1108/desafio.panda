38cef8de6b4b10e6dde88d2940e81b26221624f8
duda <duda.fucape@gmail.com>
2025-10-29 13:46:57 -0300

    atualização do menu com separação do menu e as funções em arquviso distintos

--- GestaoGastos.py
+++ GestaoGastos.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from openpyxl import Workbook
+from Funcoes import funcao
 
 wb = Workbook()
 ws = wb.active
@@ -11,49 +12,8 @@ wb.save("desafioGIT.xlsx")
 
 while True:
     print("\n================= MENU PRINCIPAL =================")
-    print(f"Bem vindo a lista de chamados, selecione o que deseja:")
-    try:
-        n = int(input(f" 1- Adicionar gastos\n 2- Vizualizar gastos\n 0- Encerrar\n"))
-
-        #Adiconar gastos:
-        if n == 1:
-            print("-----------------------------------------------------------------------")
-            data = input("Informe a data do consumo (ex: 12/10): ")
-            while True:
-                categoria = input("Informe a categoria do produto (Alimentação, Lazer, ...): ")
-                if categoria == "":
-                    print("Você deve digitar uma categoria! Tente novamente.")
-                    continue
-                break
-            while True:
-                try:
-                    valor = float(input("Informe o valor do produto: "))
-                    if valor <= 0:
-                        print("O valor deve ser positivo! Tente novamente.")
-                        continue
-                    break
-                except ValueError:
-                    print("Valor inválido! Digite apenas números.")
-            print("--------------------------GASTO ADICIONADO!---------------------------\n")
-            ws.append([data, categoria, valor])
-            wb.save("desafioGIT.xlsx")
-            continue
-
-        #Vizualizar os gastos:
-        if n == 2:
-            print("-----------------------GASTOS TOTAIS--------------------------")
-            for linha in ws.iter_rows(min_row=2, values_only=True):
-                data, categoria, valor = linha
-                print(f"-No dia {data}, foram gastos {valor} reais em {categoria}\n")
-            continue
-
-        #Encerrar codigo:
-        if n == 0:
-            print(f"encerrando...")
-            break
-
-        else:
-            print("Você deve digitar um dos numeros da lista\n")
-            continue
-    except ValueError:
-        print("voce deve digitar um número..")
+    print(f"Bem vindo a lista de chamados, selecione o que deseja: \n 1- Adicionar gastos\n 2- Vizualizar gastos\n 0- Encerrar")
+    n = int(input(f"Escolha o que deseja:"))
+    resultado = funcao(n, ws, wb)
+    if resultado == "quebra":
+        break
